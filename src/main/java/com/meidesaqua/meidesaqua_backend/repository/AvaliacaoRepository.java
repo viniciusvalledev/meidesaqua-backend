95e5e46e12deb48f608839ca198dd5ece0bd6293
Vinicius Valle dev <vnthedeveloper@gmail.com>
2025-09-04 16:51:07 -0300

    i love the users

--- src/main/java/com/meidesaqua/meidesaqua_backend/repository/AvaliacaoRepository.java
+++ src/main/java/com/meidesaqua/meidesaqua_backend/repository/AvaliacaoRepository.java
@@ -1,6 +1,7 @@
 package com.meidesaqua.meidesaqua_backend.repository;
 
 import com.meidesaqua.meidesaqua_backend.entity.Avaliacao;
+import com.meidesaqua.meidesaqua_backend.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
